3bf5fa74f4f34db31e5930b4e5e711dda3e2ced3
Justin Ginn <Justindavid.ginn@gmail.com>
2025-07-28 23:31:48 +0200

    Move osc i/o into a separate thread
    Unblock the main thread from having to do I/O

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,9 @@ use std::str::FromStr;
 use rosc::encoder;
 use rosc::{OscMessage, OscPacket, OscType};
 
+use crossbeam_channel::{bounded, Receiver, Sender};
+use std::thread;
+
 fn guid_to_string(guid: reaper_low::raw::GUID) -> String {
     format!(
         "{:08x}-{:04x}-{:04x}-{:02x}{:02x}-{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}",
@@ -39,8 +42,7 @@ fn get_id_guid(reaper: &Reaper, track: reaper_medium::MediaTrack) -> (f64, Strin
 
 #[derive(Debug)]
 struct ArpadSurface {
-    dev_addr: SocketAddrV4,
-    sock: UdpSocket,
+    osc_sender: Sender<OscPacket>,
     reaper: Reaper,
 }
 
@@ -49,13 +51,11 @@ impl ControlSurface for ArpadSurface {
         for i in 0..self.reaper.count_tracks(CurrentProject) {
             let track = self.reaper.get_track(CurrentProject, i).unwrap();
             let (track_num, track_id) = get_id_guid(&self.reaper, track);
-            let msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
-                addr: format!("/track/{}/index", track_id).to_string(),
-                args: vec![OscType::Int(track_num as i32)],
-            }))
-            .unwrap();
-            self.sock
-                .send_to(msg_buf.as_slice(), self.dev_addr)
+            self.osc_sender
+                .send(OscPacket::Message(OscMessage {
+                    addr: format!("/track/{}/index", track_id).to_string(),
+                    args: vec![OscType::Int(track_num as i32)],
+                }))
                 .unwrap();
             unsafe {
                 for i in 0..self
@@ -71,13 +71,11 @@ impl ControlSurface for ArpadSurface {
                         )
                         .unwrap();
                     let (_, dest_id) = get_id_guid(&self.reaper, dest);
-                    let msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
-                        addr: format!("/track/{}/send/{}", track_id, i).to_string(),
-                        args: vec![OscType::String(dest_id)],
-                    }))
-                    .unwrap();
-                    self.sock
-                        .send_to(msg_buf.as_slice(), self.dev_addr)
+                    self.osc_sender
+                        .send(OscPacket::Message(OscMessage {
+                            addr: format!("/track/{}/send/{}", track_id, i).to_string(),
+                            args: vec![OscType::String(dest_id)],
+                        }))
                         .unwrap();
                 }
             }
@@ -85,41 +83,46 @@ impl ControlSurface for ArpadSurface {
     }
     fn set_surface_volume(&self, args: reaper_medium::SetSurfaceVolumeArgs) {
         let (_, track_id) = get_id_guid(&self.reaper, args.track);
-        let msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
-            addr: format!("/track/{}/volume", track_id).to_string(),
-            args: vec![OscType::Float(args.volume.into_inner() as f32)],
-        }))
-        .unwrap();
-        self.sock
-            .send_to(msg_buf.as_slice(), self.dev_addr)
+        self.osc_sender
+            .send(OscPacket::Message(OscMessage {
+                addr: format!("/track/{}/volume", track_id).to_string(),
+                args: vec![OscType::Float(args.volume.into_inner() as f32)],
+            }))
             .unwrap();
     }
     fn set_surface_pan(&self, args: reaper_medium::SetSurfacePanArgs) {
         let (_, track_id) = get_id_guid(&self.reaper, args.track);
-        let msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
-            addr: format!("/track/{}/pan", track_id).to_string(),
-            args: vec![OscType::Float(args.pan.into_inner() as f32)],
-        }))
-        .unwrap();
-        self.sock
-            .send_to(msg_buf.as_slice(), self.dev_addr)
+        self.osc_sender
+            .send(OscPacket::Message(OscMessage {
+                addr: format!("/track/{}/pan", track_id).to_string(),
+                args: vec![OscType::Float(args.pan.into_inner() as f32)],
+            }))
             .unwrap();
     }
     fn set_surface_mute(&self, args: reaper_medium::SetSurfaceMuteArgs) {
         let (_, track_id) = get_id_guid(&self.reaper, args.track);
-        let msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
-            addr: format!("/track/{}/mute", track_id).to_string(),
-            args: vec![OscType::Int(if args.is_mute { 1 } else { 0 })],
-        }))
-        .unwrap();
-        self.sock
-            .send_to(msg_buf.as_slice(), self.dev_addr)
+        self.osc_sender
+            .send(OscPacket::Message(OscMessage {
+                addr: format!("/track/{}/mute", track_id).to_string(),
+                args: vec![OscType::Int(if args.is_mute { 1 } else { 0 })],
+            }))
             .unwrap();
     }
 }
 
 fn get_addr_from_arg(arg: &str) -> SocketAddrV4 {
     SocketAddrV4::from_str(arg).unwrap()
+}
+
+// Spawn the OSC sending thread
+fn start_sender_thread(dev_addr: SocketAddrV4, sock: UdpSocket, osc_receiver: Receiver<OscPacket>) {
+    thread::spawn(move || {
+        for msg in osc_receiver.iter() {
+            if let Ok(buf) = encoder::encode(&msg) {
+                let _ = sock.send_to(buf.as_slice(), dev_addr);
+            }
+        }
+    });
 }
 
 const HOST_ADDR: &str = "0.0.0.0:9090";
@@ -130,12 +133,13 @@ fn plugin_main(context: PluginContext) -> Result<(), Box<dyn Error>> {
     let host_addr = get_addr_from_arg(HOST_ADDR);
     let dev_addr = get_addr_from_arg(DEVICE_ADDR);
     let sock = UdpSocket::bind(host_addr).unwrap();
+    let (osc_sender, osc_receiver) = bounded(128); // buffer size as needed
+    start_sender_thread(dev_addr, sock.try_clone().unwrap(), osc_receiver);
 
     let mut session = reaper_medium::ReaperSession::load(context);
     let reaper = session.reaper();
     let mut arpad = ArpadSurface {
-        sock,
-        dev_addr,
+        osc_sender,
         reaper: reaper.clone(),
     };
     arpad.run();
